25a78f35c8ed145964860f6b2279d2c94eb0c7e5
Douangdeuan <114629867+Douangdeuan@users.noreply.github.com>
2022-10-12 20:57:21 +0700

    Add files via upload

---
+++ src/BaitapPizza/Circle.java
@@ -0,0 +1,22 @@
+package BaitapPizza;
+
+public class Circle implements IShape{
+    double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                '}';
+    }
+
+}
---
+++ src/BaitapPizza/IShape.java
@@ -0,0 +1,5 @@
+package BaitapPizza;
+
+public interface IShape {
+    double getArea();
+}
